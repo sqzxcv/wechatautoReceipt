45e00a176bafb37279d27ee2d9c7501693cc41e0
ShengQiang <qiang_sheng@qq.com>
2017-06-26 19:40:18 +0800

    更改数据库配置

--- config/production.js
+++ config/production.js
@@ -1,6 +1,9 @@
 var config = {
-    "dbhost":"localhost",
-    "dbuser":"root",
-    "dbpwd":"Anhuiqiang851"
+    // "dbhost":"localhost",
+    // "dbuser":"root",
+    // "dbpwd":"Anhuiqiang851"
+    "dbhost": "172.104.91.83",
+    "dbuser": "admin",
+    "dbpwd": "Anhuiqiang851",
 };
 module.exports = config;
